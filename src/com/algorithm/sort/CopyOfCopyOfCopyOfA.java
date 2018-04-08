1ca0d8e3c643856b8bbc5e08fd3ac877c02e1778
anamika11 <anamikaprasad11@gmail.com>
2018-04-08 11:22:32 +0530

    Update CopyOfCopyOfCopyOfA.java

--- src/com/algorithm/sort/CopyOfCopyOfCopyOfA.java
+++ src/com/algorithm/sort/CopyOfCopyOfCopyOfA.java
@@ -1,5 +1,30 @@
 package com.algorithm.sort;
 
-public class CopyOfCopyOfCopyOfA {
+public class SelectionSort {
 
+	public static void main(String[] args) {
+		//int []n = {56,2,34,89,12,46};
+		//selectionSort(n);
+		int [] sortedArray = selectionSort(new int []{56,2,34,89,12,46});
+		for (int i = 0; i < sortedArray.length; i++) {
+			System.out.println(sortedArray[i]);
+		}
+	}
+
+	public static int[] selectionSort(int [] arrayToSort){
+		
+		for (int i = 0; i < arrayToSort.length; i++) {
+			int minimum=i;
+			for (int j = i+1; j < arrayToSort.length; j++) {
+				if(arrayToSort[j]<arrayToSort[minimum]){
+					minimum=j;
+				}
+			}
+			int temp=arrayToSort[i];
+			arrayToSort[i]=arrayToSort[minimum];
+			arrayToSort[minimum]=temp;
+		}
+		
+		return arrayToSort;
+	}
 }
