3a3bf0f68a594c7c5c9a48af0d51ed7f4c9d6ba5
anamika11 <anamikaprasad11@gmail.com>
2018-04-08 11:19:36 +0530

    Update A.java

--- src/com/algorithm/search/A.java
+++ src/com/algorithm/search/A.java
@@ -1,5 +1,25 @@
 package com.algorithm.search;
 
-public class A {
-
+public class BinarySearchAlg {
+	
+	public static void main(String[] args) {
+		int[] a={2,4,6,8,12,45,50,65,70,75,80};
+		System.out.print(binarySearch(a, 45));
+	}
+	
+	public static int binarySearch(int[] searchHere, int value ){
+		int p= 0;int r=searchHere.length-1;
+		while(p<=r){
+			int q=(p+r)/2;
+			if(searchHere[q]==value){
+				return q;
+			}else if(searchHere[q]>value){
+				r=q-1;
+			}else{
+				p=q+1;
+			}
+		}
+		
+		return 0;
+	}
 }
