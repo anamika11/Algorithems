b58cedc78bbfc683df471f9d85b6e2381302bd03
anamika11 <anamikaprasad11@gmail.com>
2018-04-08 11:20:12 +0530

    Update CopyOfA.java

--- src/com/algorithm/search/CopyOfA.java
+++ src/com/algorithm/search/CopyOfA.java
@@ -1,5 +1,19 @@
 package com.algorithm.search;
 
-public class CopyOfA {
+public class LinearSearchAlg {
 
+	public static void main(String[] args) {
+		int[] a={2,4,6,8,12,45};
+		System.out.print(linearSearch(a, 45));
+	}
+	
+	public static int linearSearch(int[] searchHere, int value ){
+		for (int i=0;i<searchHere.length;i++){
+			if(searchHere[i]== value){
+				return i;
+			}
+		}	
+		return -1;
+	}
+	
 }
