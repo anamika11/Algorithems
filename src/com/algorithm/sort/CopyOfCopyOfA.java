8abe3aa8b4fa796bdbd17d5247b8ee01ac7000c0
anamika11 <anamikaprasad11@gmail.com>
2018-04-08 11:22:14 +0530

    Update CopyOfCopyOfA.java

--- src/com/algorithm/sort/CopyOfCopyOfA.java
+++ src/com/algorithm/sort/CopyOfCopyOfA.java
@@ -1,5 +1,15 @@
 package com.algorithm.sort;
 
-public class CopyOfCopyOfA {
+public class MergeSortMain {
+
+	public static void main(String[] args) {
+     int [] a = {8,5,78,56,99,12,45,65,34,25,9};
+		MergeSortApp sorter= new MergeSortApp();
+		sorter.sort(a);
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
 
 }
