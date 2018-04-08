b46db6e018a46fabf2d44c0899db0981bfe31d5c
anamika11 <anamikaprasad11@gmail.com>
2018-04-08 11:21:20 +0530

    Update CopyOfCopyOfCopyOfCopyOfA.java

--- src/com/algorithm/search/CopyOfCopyOfCopyOfCopyOfA.java
+++ src/com/algorithm/search/CopyOfCopyOfCopyOfCopyOfA.java
@@ -1,5 +1,17 @@
 package com.algorithm.search;
 
-public class CopyOfCopyOfCopyOfCopyOfA {
+public class RecursiveWay {
 
+	public static void main(String[] args) {
+		recursiveMethod(10);
+	}
+
+	
+	public static void recursiveMethod(int n){
+		if(n>=0)
+			recursiveMethod(n-1);
+		
+		System.out.println("value :"+n);  // The way n is printed this way because it is a recursive method call. In a stack the method call 
+		//keeps on happening and method grow on top of other, so first the top level method will be executed and then bottom level
+	}
 }
