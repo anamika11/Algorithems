6fd779488704b15d6d659a3156fb97c3c5b8b36c
anamika11 <anamikaprasad11@gmail.com>
2018-04-08 11:20:37 +0530

    Update CopyOfCopyOfA.java

--- src/com/algorithm/search/CopyOfCopyOfA.java
+++ src/com/algorithm/search/CopyOfCopyOfA.java
@@ -1,5 +1,23 @@
 package com.algorithm.search;
 
-public class CopyOfCopyOfA {
+public class RecursiveBinarySearch {
 
+	public static void main(String[] args) {
+		int a[]={10,20,12,45,60,46};
+		System.out.print(recursiveLinearSearch(a,0,45));
+	}
+
+	
+	public static int recursiveLinearSearch(int[] search,int i, int x){
+		if(i>search.length-1){
+			return -1;
+		}else if(search[i]==x){
+			return i;
+			
+		}else{
+			System.out.println("value :"+i);
+			return recursiveLinearSearch(search, i+1, x);
+		}
+		
+	}
 }
